5a0d8280d1aa19931b18e848f414c02d6c8fe4be
Michael Madden <mikemadden42@icloud.com>
2022-03-24 22:41:27 -0500

    For this trivial app, ignore the warning about pseudo random numbers.

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	words := []string{"begin", "great", "suddenly", "before", "enough", "probably"}
 	rand.Seed((time.Now().UnixNano()))
 	length := len(words)
-	index := rand.Intn(length - 1)
+	index := rand.Intn(length - 1) //nolint:gosec // trade less secure pseudo random numbers for faster speeds
 	word := words[index]
 	fmt.Println(word)
 }
