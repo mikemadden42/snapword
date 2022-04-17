1a1a3fe4e1b74f62edce85583ec0012112c9bdec
Michael Madden <mikemadden42@icloud.com>
2022-04-17 08:30:03 -0500

    Remove extra parens.

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	words := []string{"begin", "great", "suddenly", "before", "enough", "probably"}
-	rand.Seed((time.Now().UnixNano()))
+	rand.Seed(time.Now().UnixNano())
 	length := len(words)
 	index := rand.Intn(length - 1) //nolint:gosec // trade less secure pseudo random numbers for faster speeds
 	word := words[index]
