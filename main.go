ae53543dca5247c145be654ee7068768cb5bc541
Michael Madden <mikemadden42@icloud.com>
2022-07-10 16:12:00 -0500

    Break out getWord function.

--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 )
 
 func main() {
-	words := []string{"begin", "great", "suddenly", "before", "enough", "probably"}
+	words := []string{"begin", "great", "suddenly", "before", "enough", "probably", "amaze", "exercise", "curious", "friendly", "enormous"}
+	word := getWord(words)
+	fmt.Println(word)
+}
+
+func getWord(words []string) string {
 	rand.Seed(time.Now().UnixNano())
 	length := len(words)
 	index := rand.Intn(length - 1) //nolint:gosec // trade less secure pseudo random numbers for faster speeds
 	word := words[index]
-	fmt.Println(word)
+	return word
 }
